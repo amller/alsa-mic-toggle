d44cb4da6e6394f7666843f66ccf3402af7c67ce
amller <arthurmueller95@gmail.com>
2025-12-05 22:44:06 +0100

    Do not use cd

--- mic-toggle.sh
+++ mic-toggle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd $(dirname "$0")
+DIR=$(dirname "$0")
 amixer set Capture toggle
 STATUS=$(amixer get Capture | tail --lines=1 | grep --fixed-strings --only-matching "[on]")
 test "$STATUS" = "[on]" && AUDIOFILE="unmute.wav" || AUDIOFILE="mute.wav"
-aplay --quiet $AUDIOFILE
+aplay --quiet $DIR/$AUDIOFILE
 
